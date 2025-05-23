c97cdac8d79f9818ba4a19c8864602cdb32e30ba
unknown <User@600000-96.ad.lan>
2025-05-23 14:09:34 +0530

    changes

--- AuthAPI/Models/Note.cs
+++ AuthAPI/Models/Note.cs
@@ -5,7 +5,7 @@ public class Note
     public int Id { get; set; }
 
     [Required]
-    public string Title { get; set; }
+    public  string Title { get; set; }
 
     public string Content { get; set; }
 }
--- AuthAPI/obj/Debug/net9.0/AuthAPI.AssemblyInfo.cs
+++ AuthAPI/obj/Debug/net9.0/AuthAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+061db6d1ffc38b79bcd275ae5606db5bb150c308")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
